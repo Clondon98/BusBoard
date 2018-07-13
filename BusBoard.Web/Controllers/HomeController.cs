becec37e66d1f79aeaa7021d77ba49961d6d405e
Charles London <Charles.London@softwire.com>
2018-07-13 14:26:42 +0100

    null reference fix

--- BusBoard.Web/Controllers/HomeController.cs
+++ BusBoard.Web/Controllers/HomeController.cs
@@ -25,11 +25,12 @@ namespace BusBoard.Web.Controllers
       // Add some properties to the BusInfo view model with the data you want to render on the page.
       // Write code here to populate the view model with info from the APIs.
       // Then modify the view (in Views/Home/BusInfo.cshtml) to render upcoming buses.
-      string postcode = selection.Postcode.Replace(" ", String.Empty).ToUpper();
+      string postcode;
       List<ConsoleApp.BusInfo> buses;
 
       try
       {
+        postcode = selection.Postcode.Replace(" ", String.Empty).ToUpper();
         buses = busInfoList(postcode);
       }
       catch (NullReferenceException)
